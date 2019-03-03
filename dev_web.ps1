71a1ffaddbb402f848a9041d3b2db5e30e1c1a93
Kari Häkkinen <hakkika@gmail.com>
2019-03-03 20:34:07 +0200

    Removed Hyper-V and WSL

--- dev_web.ps1
+++ dev_web.ps1
@@ -26,9 +26,9 @@ executeScript "FileExplorerSettings.ps1";
 executeScript "SystemConfiguration.ps1";
 executeScript "CommonDevTools.ps1";
 executeScript "RemoveDefaultApps.ps1";
-executeScript "HyperV.ps1";
+# executeScript "HyperV.ps1";
 executeScript "Docker.ps1";
-executeScript "WSL.ps1";
+# executeScript "WSL.ps1";
 executeScript "Browsers.ps1";
 
 #--- Tools ---
